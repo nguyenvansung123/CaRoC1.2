4d40296d09336e3ce4d9d6301d91a7e8d01b01e2
nguyentanloc200495 <nguyentanloc.200495@gmail.com>
2016-12-27 11:07:10 +0700

    them nhac vao game

--- CaRoC/Form1.cs
+++ CaRoC/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +19,8 @@ namespace CaRoC
         public frmCaro()
         {
             InitializeComponent();
+            SoundPlayer Sound = new SoundPlayer("C://Users//My-PC//CaRoC1.2//CaRoC//Resources//nhacnen.wav");
+            Sound.Play();
             caroChess = new CaroChess();
             grs = pnBanCo.CreateGraphics();
             caroChess.KhoiTaoMangOCo();
